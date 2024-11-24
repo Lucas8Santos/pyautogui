fd6f6a301a10b86c78f9a5f0227fa377338073bc
Lucas8Santos <classeviprc@gmail.com>
2024-11-24 16:53:27 -0300

    criandopasta.py

---
+++ criandopasta.py
@@ -0,0 +1,8 @@
+#desafio para criar pasta
+import pyautogui
+#click personalizado
+pyautogui.rightClick(1586,145, duration=2)
+pyautogui.move(20,200, duration=2)
+pyautogui.move(260,0, duration=2)
+pyautogui.click()
+
